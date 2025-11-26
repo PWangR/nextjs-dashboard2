f559a084910836a79f8fe6a449c31d2eb0945a35
PWangR <124851875+PWangR@users.noreply.github.com>
2025-11-26 08:15:24 +0700

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}
+        <p
+          className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+        > <strong>Welcome to Acme.</strong> This is the example for the{' '}
+          <a href="https://nextjs.org/learn/" className="text-blue-500">
+            Next.js Learn Course
+          </a>
+          , brought to you by Vercel.
+        </p>
+      </body>
     </html>
   );
 }
