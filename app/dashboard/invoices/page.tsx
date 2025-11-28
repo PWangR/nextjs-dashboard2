62e53314f226c94e55b74c7dc4e8ea4fc75d361c
PWangR <124851875+PWangR@users.noreply.github.com>
2025-11-28 09:58:42 +0700

    thêm metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -9,12 +9,7 @@ import { fetchInvoicesPages } from '@/app/lib/data';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: {
-        template: '%s | Acme Dashboard',
-        default: 'Acme Dashboard',
-    },
-    description: 'The official Next.js Learn Dashboard built with App Router.',
-    metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+    title: 'Invoices',
 };
 
 
